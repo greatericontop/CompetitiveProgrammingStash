86fe206cb87a1d72898e7c52aca486556cc52d5f
greatericontop <greateric@protonmail.com>
2026-03-08 10:29:17 -0400

    fix indentation in template

--- templates/dsu.cpp
+++ templates/dsu.cpp
@@ -11,36 +11,36 @@ using namespace std;
  */
 class DSU {
 private:
-	vector<int> parents;
-	vector<int> sizes;
+  vector<int> parents;
+  vector<int> sizes;
 
 public:
-	int number_cc;
+  int number_cc;
   int largest_cc;
 
   explicit DSU(int n) : parents(n), sizes(n, 1), number_cc(n), largest_cc(1) {
-		for (int i = 0; i < n; i++)  parents[i] = i;
-	}
+    for (int i = 0; i < n; i++)  parents[i] = i;
+  }
 
-	int find(int x) {
+  int find(int x) {
     return parents[x] == x ? x : (parents[x] = find(parents[x]));
   }
 
-	bool unite(int x, int y) {
-		int x_root = find(x);
-		int y_root = find(y);
-		if (x_root == y_root)  return false;
-		if (sizes[x_root] < sizes[y_root]) {
+  bool unite(int x, int y) {
+    int x_root = find(x);
+    int y_root = find(y);
+    if (x_root == y_root)  return false;
+    if (sizes[x_root] < sizes[y_root]) {
       swap(x_root, y_root);
     }
-		sizes[x_root] += sizes[y_root];
-		parents[y_root] = x_root;
+    sizes[x_root] += sizes[y_root];
+    parents[y_root] = x_root;
     largest_cc = max(largest_cc, sizes[x_root]);
     number_cc--;
-		return true;
-	}
+    return true;
+  }
 
-	bool connected(int x, int y) { return find(x) == find(y); }
+  bool connected(int x, int y) { return find(x) == find(y); }
 };
 
 
