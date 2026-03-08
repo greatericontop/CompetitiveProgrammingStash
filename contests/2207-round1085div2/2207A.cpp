60183a919e9bae4171f069fc8d468c8d732cf9c4
greatericontop <greateric@protonmail.com>
2026-03-08 10:48:51 -0400

    save

--- contests/2207-round1085div2/2207A.cpp
+++ contests/2207-round1085div2/2207A.cpp
@@ -11,8 +11,36 @@ using namespace std;
 void solve() {
   int n;
   cin >> n;
-  //
+  string s;
+  cin >> s;
 
+  // Max
+
+  string s1 = s;
+  for (int i = 1; i < n-1; i++) {
+    if (s1[i-1] == '1' && s1[i+1] == '1') {
+      s1[i] = '1';
+    }
+  }
+  int maximum = 0;
+  for (char c : s1) {
+    if (c == '1')  maximum++;
+  }
+
+  // Min
+
+  string s2 = s1; // start from max string
+  for (int i = 1; i < n-1; i++) {
+    if (s2[i-1] == '1' && s2[i+1] == '1') {
+      s2[i] = '0';
+    }
+  }
+  int minimum = 0;
+  for (char c : s2) {
+    if (c == '1')  minimum++;
+  }
+
+  cout << minimum << ' ' << maximum << '\n';
 }
 
 
