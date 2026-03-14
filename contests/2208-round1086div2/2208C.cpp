86c1bf7b99b33bbe30d86e574a2e5e189910f3c2
greatericontop <greateric@protonmail.com>
2026-03-14 10:55:32 -0400

    save

--- contests/2208-round1086div2/2208C.cpp
+++ contests/2208-round1086div2/2208C.cpp
@@ -9,7 +9,31 @@ using namespace std;
 
 
 void solve() {
-  
+  int n;
+  cin >> n;
+  vector<int> values(n+1);
+  vector<int> difficulties(n+1);
+  for (int i = 1; i <= n; i++) {
+    cin >> values[i];
+    cin >> difficulties[i];
+  }
+
+  // Rec spec (hi cs381)
+  // dp[i] = the best score you can get by processing tasks i...n, assuming a starting stamina of 1.
+  // If your starting stamina is 0.6767 for example, then you have to multiply by 0.6767
+  vector<double> dp(n+1);
+  // base case: just do the last task
+  dp[n] = (double) values[n];
+  for (int i = n-1; i >= 1; i--) {
+    // Ignore self
+    double ignore_self = dp[i+1];
+    // Do self
+    double new_stamina_after_self = 1.0 - (difficulties[i]/100.0);
+    double do_self = values[i] + new_stamina_after_self * dp[i+1];
+    dp[i] = max(ignore_self, do_self);
+  }
+
+  cout << fixed << setprecision(12) << dp[1] << "\n";
 }
 
 
