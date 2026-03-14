1085dd7b007155c9ec4f9e04509c96d74b6791c0
greatericontop <greateric@protonmail.com>
2026-03-14 10:38:17 -0400

    save

--- contests/2208-round1086div2/2208A.cpp
+++ contests/2208-round1086div2/2208A.cpp
@@ -9,7 +9,27 @@ using namespace std;
 
 
 void solve() {
+  int n;
+  cin >> n;
+  vector<int> board(n*n);
+  for (int i = 0; i < n*n; i++) {
+    cin >> board[i];
+  }
+  map<int, int> freq;
+  for (int i = 0; i < n*n; i++) {
+    freq[board[i]]++;
+  }
 
+  int highest_freq = 0;
+  for (auto& [_, f] : freq) {
+    highest_freq = max(highest_freq, f);
+  }
+
+  if (highest_freq <= n*(n-1)) {
+    cout << "YES\n";
+  } else {
+    cout << "NO\n";
+  }
 }
 
 
