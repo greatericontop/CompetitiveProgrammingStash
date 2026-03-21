1862f7c048ed13128b189df34a28e2d67236e92a
greatericontop <greateric@protonmail.com>
2026-03-21 11:04:04 -0400

    save

--- contests/2209-round1087div2/2209C.cpp
+++ contests/2209-round1087div2/2209C.cpp
@@ -11,8 +11,50 @@ using namespace std;
 void solve() {
   int n;
   cin >> n;
-  //
+  // Hidden array length 2n (at least length 4)
 
+  for (int i = 5; i <= 2 * n; i += 2) {
+    // 5 6, 7 8, ...
+    cout << "? " << i << " " << (i + 1) << endl;
+    int x;
+    cin >> x;
+    assert(x != -1);
+    if (x == 1) {
+      cout << "! " << i << endl;
+      return;
+    }
+  }
+
+  // [1 2 3 4], 3 queries left
+  cout << "? 1 2" << endl;
+  int x;
+  cin >> x;
+  assert(x != -1);
+  if (x == 1) {
+    cout << "! 1" << endl;
+    return;
+  }
+
+  cout << "? 1 3" << endl;
+  int y;
+  cin >> y;
+  assert(y != -1);
+  if (y == 1) {
+    cout << "! 1" << endl;
+    return;
+  }
+
+  cout << "? 2 3" << endl;
+  int z;
+  cin >> z;
+  assert(z != -1);
+  if (z == 1) {
+    cout << "! 2" << endl;
+    return;
+  }
+
+  cout << "! 4" << endl;
+  return;
 }
 
 
