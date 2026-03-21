c25ddc42e1ae02e123befc1de7383dbb063d68a8
greatericontop <greateric@protonmail.com>
2026-03-21 10:45:56 -0400

    save

--- contests/2209-round1087div2/2209B.cpp
+++ contests/2209-round1087div2/2209B.cpp
@@ -11,7 +11,34 @@ using namespace std;
 void solve() {
   int n;
   cin >> n;
-  //
+  vector<int> A(n);
+  for (int i = 0; i < n; i++)  cin >> A[i];
+
+  vector<int> answers(n);
+  multiset<int> processed;
+
+  answers[n-1] = 0;
+  processed.insert(A[n-1]);
+
+  for (int i = n - 2; i >= 0; i--) {
+    // Can use OST for faster runtime
+    int less_than_us = 0;
+    int more_than_us = 0;
+    for (int x : processed) {
+      if (x < A[i]) {
+        less_than_us++;
+      } else if (x > A[i]) { // not equal
+        more_than_us++;
+      }
+    }
+    answers[i] = max(less_than_us, more_than_us);
+    processed.insert(A[i]);
+  }
+
+  for (int i = 0; i < n; i++) {
+    cout << answers[i] << " ";
+  }
+  cout << "\n";
 
 }
 
