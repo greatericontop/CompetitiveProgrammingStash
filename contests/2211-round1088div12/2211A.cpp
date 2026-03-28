dc8d579fb9ef0842f9c19cf373e613c8c9151ed4
greatericontop <greateric@protonmail.com>
2026-03-28 10:48:16 -0400

    A

--- contests/2211-round1088div12/2211A.cpp
+++ contests/2211-round1088div12/2211A.cpp
@@ -11,8 +11,23 @@ using namespace std;
 void solve() {
   int n;
   cin >> n;
-  //
+  vector<int> a(n);
+  for (int i = 0; i < n; i++)  cin >> a[i];
 
+  if (n == 1) {
+    cout << "1\n";
+    return;
+  }
+  if (n == 2) {
+    cout << "2 2\n";
+    return;
+  }
+  if (n >= 3) {
+    for (int i = 0; i < n; i++) {
+      cout << "2 ";
+    }
+    cout << "\n";
+  }
 }
 
 
