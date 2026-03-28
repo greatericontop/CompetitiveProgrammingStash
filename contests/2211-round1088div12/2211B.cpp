dd87c247a0123d8f108cc68668d4f9a4e0c79bf8
greatericontop <greateric@protonmail.com>
2026-03-28 10:54:23 -0400

    B

--- contests/2211-round1088div12/2211B.cpp
+++ contests/2211-round1088div12/2211B.cpp
@@ -9,9 +9,26 @@ using namespace std;
 
 
 void solve() {
-  int n;
-  cin >> n;
-  //
+  int x, y;
+  cin >> x >> y;
+
+  int S = abs(x - y);
+  // Ans = # of factors of S
+  // S = 2 ---> 1, 2 ---> ans of 2
+  int factors = 0;
+  for (int i = 1; i <= S; i++) {
+    if (S % i == 0)  factors++;
+  }
+  if (S == 0)  factors = 1;
+
+  cout << factors << "\n";
+  for (int i = 0; i < x; i++) {
+    cout << "1 ";
+  }
+  for (int i = 0; i < y; i++) {
+    cout << "-1 ";
+  }
+  cout << "\n";
 
 }
 
