be266257f871e9aaa9fbecb7defe2796250d7cdc
greatericontop <greateric@protonmail.com>
2026-03-29 10:48:50 -0400

    save

--- contests/2210-round1089div2/2210B.cpp
+++ contests/2210-round1089div2/2210B.cpp
@@ -11,7 +11,29 @@ using namespace std;
 void solve() {
   int n;
   cin >> n;
-  //
+  vector<int> A(n+1);
+  vector<int> reverse(n+1);
+  for (int i = 1; i <= n; i++) {
+    cin >> A[i];
+    reverse[A[i]] = i;
+  }
+
+  int best = 0;
+  set<int> elements;
+  for (int i = 1; i <= n; i++) {
+    // Number of elements 1...i that are >i
+    // Remove i
+    int index_of_i = reverse[i];
+    if (elements.find(index_of_i) != elements.end()) {
+      fprintf(stderr, "  removing %d\n", index_of_i);
+      elements.erase(index_of_i);
+    }
+    elements.insert(i);
+    fprintf(stderr, "i=%d, elements %d\n", i, (int)elements.size());
+    best = max(best, (int)elements.size());
+  }
+
+  cout << best << "\n";
 
 }
 
