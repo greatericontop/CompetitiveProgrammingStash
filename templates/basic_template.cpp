d7aa223f03173f2e1a7eee5916f1fc9d431b055a
greatericontop <greateric@protonmail.com>
2026-04-23 20:29:42 -0400

    make a hopefully good template

--- templates/basic_template.cpp
+++ templates/basic_template.cpp
@@ -2,7 +2,41 @@
 using namespace std;
 #define long long long
 
-#define fprintf(...) // no-op
+
+#define GREATERIC_DEBUG
+#define STRESSTESTING
+
+
+#ifdef GREATERIC_DEBUG
+  #define PRINTVEC(vec) \
+  do { \
+    fprintf(stderr, "%s:  ", #vec); \
+    for (const auto& _x : vec)  fprintf(stderr, "%d ", _x); \
+    fprintf(stderr, "\n"); \
+  } while (0)
+  #define PRINTMAP(map) \
+  do { \
+    fprintf(stderr, "%s:  ", #map); \
+    for (const auto& _p : map)  fprintf(stderr, "%d->%d ", _p.first, _p.second); \
+    fprintf(stderr, "\n"); \
+  } while (0)
+  #pragma GCC diagnostic error "-Wall"
+  #pragma GCC diagnostic error "-Wextra"
+  #pragma GCC diagnostic error "-Wshadow"
+  #pragma GCC diagnostic error "-Wconversion"
+  #pragma GCC diagnostic error "-Wfloat-equal"
+  #pragma GCC diagnostic error "-Wduplicated-cond"
+  #pragma GCC diagnostic error "-Wlogical-op"
+#else
+  #define fprintf(...) // no-op
+  #define PRINTVEC(...) // no-op
+  #define PRINTMAP(...) // no-op
+#endif
+
+
+
+
+
 
 
 
@@ -19,11 +53,33 @@ void solve() {
 
 
 
+
+
+
+
+
+void stresstest() {
+  mt19937_64 rng((uint64_t)chrono::steady_clock::now().time_since_epoch().count());
+  //int n = 100'000;
+  //...
+  //solve();
+}
+
+
 int main() {
   ios::sync_with_stdio(false);
   cin.tie(nullptr);
+#ifdef STRESSTESTING
+  fprintf(stderr, "Starting stresstest\n");
+  static constexpr int RUNS = 100'000;
+  for (int bundle = 1; true; bundle++) {
+    for (int i = 0; i < RUNS; i++)  stresstest();
+    fprintf(stderr, "Bundle %d, completed %d runs\n", bundle, RUNS);
+  }
+#else
   int t;
   cin >> t;
   while (t--)  solve();
+#endif
   return 0;
 }
