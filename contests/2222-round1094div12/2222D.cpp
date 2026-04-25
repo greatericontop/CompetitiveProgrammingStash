0b7212ba09379e05e3978da146e82b35a73abeb0
greatericontop <greateric@protonmail.com>
2026-04-25 09:51:32 -0400

    init

---
+++ contests/2222-round1094div12/2222D.cpp
@@ -0,0 +1,66 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define long long long
+
+
+#define GREATERIC_DEBUG
+
+
+#ifdef GREATERIC_DEBUG
+  #define PRINTVEC(vec) do { \
+    fprintf(stderr, "%s:  ", #vec); \
+    for (const auto& _x : (vec))  fprintf(stderr, "%d ", _x); \
+    fprintf(stderr, "\n"); \
+  } while (0)
+  #define PRINTMAP(map) do { \
+    fprintf(stderr, "%s:  ", #map); \
+    for (const auto& _p : (map))  fprintf(stderr, "%d->%d ", _p.first, _p.second); \
+    fprintf(stderr, "\n"); \
+  } while (0)
+  #define DEBUGFOREACH(vec, stmt) do { \
+    fprintf(stderr, "%s:  ", #vec); \
+    for (const auto& x : (vec)) { \
+      stmt; \
+    } \
+  } while (0)
+#else
+  #define fprintf(...) // no-op
+  #define PRINTVEC(...) // no-op
+  #define PRINTMAP(...) // no-op
+#endif
+
+
+
+
+
+
+
+
+
+
+void solve() {
+  int n;
+  cin >> n;
+  //
+  // Don't forget to enable compiler options
+  //     //
+
+}
+
+
+
+
+
+
+
+
+
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  int t;
+  cin >> t;
+  while (t--)  solve();
+  return 0;
+}
