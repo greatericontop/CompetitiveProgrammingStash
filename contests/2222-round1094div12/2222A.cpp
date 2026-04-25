bc084b5ac425210c895ec6153dfa2237f1951d37
greatericontop <greateric@protonmail.com>
2026-04-25 10:38:47 -0400

    save

--- contests/2222-round1094div12/2222A.cpp
+++ contests/2222-round1094div12/2222A.cpp
@@ -41,9 +41,16 @@ using namespace std;
 void solve() {
   int n;
   cin >> n;
-  //
-  // Don't forget to enable compiler options
-  //     //
+  vector<int> a(n);
+  for (int i = 0; i < n; i++)  cin >> a[i];
+
+  for (int i = 0; i < n; i++) {
+    if (a[i] == 100) {
+      cout << "YES\n";
+      return;
+    }
+  }
+  cout << "NO\n";
 
 }
 
