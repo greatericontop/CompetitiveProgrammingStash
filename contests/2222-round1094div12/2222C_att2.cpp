1e850dd5afecb6a5f5969e5db07691a0a04260aa
greatericontop <greateric@protonmail.com>
2026-04-25 12:08:04 -0400

    save

--- contests/2222-round1094div12/2222C_att2.cpp
+++ contests/2222-round1094div12/2222C_att2.cpp
@@ -64,7 +64,7 @@ void solve() {
       else if (a[i] > med)  ct_r++;
       else  ct_m++;
 
-      if ((ct_l + ct_r + ct_m) % 2 == 1 && (ct_m >= 1) && (ct_l + ct_m > ct_r) && (ct_r + ct_m > ct_l)) {
+      if ((ct_l + ct_r + ct_m) % 2 == 1 && (ct_m >= 1) && ((ct_l + ct_m > ct_r) || (ct_r + ct_m > ct_l))) {
         cur_ans++;
         ct_l = 0;
         ct_r = 0;
@@ -72,8 +72,10 @@ void solve() {
       }
     }
     if (ct_l != 0 || ct_r != 0 || ct_m != 0) {
-      // incomplete
-      continue;
+      if (ct_l != ct_r) {
+        // last one can be extended
+        continue;
+      }
     }
     best_ans = max(best_ans, cur_ans);
   }
--- contests/2222-round1094div12/c_test_gen.py
+++ contests/2222-round1094div12/c_test_gen.py
@@ -3,7 +3,7 @@
 import random
 
 print(1)
-n = 979
+n = 199
 print(n)
 print(' '.join(str(random.randint(1, 1500)) for _ in range(n)))
 
