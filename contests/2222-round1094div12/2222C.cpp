5a4a30ddb55330379a3cf0495db34e4e921ef5cd
greatericontop <greateric@protonmail.com>
2026-04-25 11:19:02 -0400

    save

--- contests/2222-round1094div12/2222C.cpp
+++ contests/2222-round1094div12/2222C.cpp
@@ -3,7 +3,7 @@ using namespace std;
 #define long long long
 
 
-#define GREATERIC_DEBUG
+//#define GREATERIC_DEBUG
 
 
 #ifdef GREATERIC_DEBUG
@@ -41,9 +41,48 @@ using namespace std;
 void solve() {
   int n;
   cin >> n;
-  //
-  // Don't forget to enable compiler options
-  //     //
+  vector<int> a(n+1);
+  for (int i = 1; i <= n; i++)  cin >> a[i];
+  map<int, int> first_occurrence;
+  for (int i = 1; i <= n; i++) {
+    if (first_occurrence[a[i]] == 0)  first_occurrence[a[i]] = i;
+  }
+  PRINTMAP(first_occurrence);
+//  vector<int> to_dp_index(n+1);
+//  for (int i = 1; i <= n; i++) {
+//    to_dp_index[i] = first_occurrence[a[i]];
+//  }
+  //PRINTVEC(to_dp_index);
+
+  vector<vector<int>> dp(n+1, vector<int>(n+1, -1));
+  dp[0] = vector<int>(n+1, 0); // 0-length subarrays for each median, base case
+
+  for (int i = 1; i <= n; i++) {
+    vector<int> nums;
+    nums.push_back(a[i]);
+    for (int j = i; j >= 1; j -= 2) {
+      nth_element(nums.begin(), nums.begin() + nums.size()/2, nums.end());
+      int cur_median = nums[nums.size()/2];
+      int cur_median_dp_index = first_occurrence[cur_median];    // n log n?
+      assert(cur_median_dp_index <= n);
+      if (dp[j-1][cur_median_dp_index] != -1) {
+        dp[i][cur_median_dp_index] = max(dp[i][cur_median_dp_index], dp[j - 1][cur_median_dp_index] + 1);
+      }
+      // transition to next
+      if (j - 2 >= 1) {
+        nums.push_back(a[j-1]);
+        nums.push_back(a[j-2]);
+      }
+    }
+  }
+
+  int ans = -1;
+  for (int x : dp[n]) {
+    assert(x == -1 || (x % 2 == 1));
+    ans = max(ans, x);
+  }
+  assert(ans >= 1);
+  cout << ans << "\n";
 
 }
 
