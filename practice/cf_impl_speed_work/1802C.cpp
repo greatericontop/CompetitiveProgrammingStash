908993d507a71f4af212373a0393cc9bef3be439
greatericontop <greateric@protonmail.com>
2026-04-26 15:17:36 -0400

    save

---
+++ practice/cf_impl_speed_work/1802C.cpp
@@ -0,0 +1,70 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define long long long
+
+
+#define GREATERIC_DEBUG
+
+
+#ifdef GREATERIC_DEBUG
+  #define PRINTVEC(vec) do { \
+    fprintf(stderr, "%s:  ", #vec); \
+    for (const auto& _x : (vec))  fprintf(stderr, "%d ", _x); \
+    fprintf(stderr, "\n"); \
+  } while (0)
+  #define PRINTMAP(map) do { \
+    fprintf(stderr, "%s:  ", #map); \
+    for (const auto& _p : (map))  fprintf(stderr, "%d->%d ", _p.first, _p.second); \
+    fprintf(stderr, "\n"); \
+  } while (0)
+  #define DEBUGFOREACH(vec, stmt) do { \
+    fprintf(stderr, "%s:  ", #vec); \
+    for (const auto& x : (vec)) { \
+      stmt; \
+    } \
+  } while (0)
+#else
+  #define fprintf(...) // no-op
+  #define PRINTVEC(...) // no-op
+  #define PRINTMAP(...) // no-op
+#endif
+
+
+
+
+
+
+
+
+
+
+void solve() {
+  int n, m;
+  cin >> n >> m;
+
+  cout << (n * m) << "\n";
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j < m; j++) {
+      cout << (256 * i + j) << " \n"[j == m - 1];
+    }
+  }
+
+}
+
+
+
+
+
+
+
+
+
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  int t;
+  cin >> t;
+  while (t--)  solve();
+  return 0;
+}
