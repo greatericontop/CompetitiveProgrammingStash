4c18b87766277c8a0a7228db69feeb182d6bfb17
greatericontop <greateric@protonmail.com>
2026-05-01 19:15:53 -0400

    save

---
+++ practice/cf_impl_speed_work/2085C.cpp
@@ -0,0 +1,77 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define long long long
+
+
+#define GREATERIC_DEBUG
+
+
+#ifdef GREATERIC_DEBUG
+  #define PRINTVEC(vec) do { \
+    fprintf(stderr, "%s:  ", #vec); \
+    for (const auto& _x : (vec))  fprintf(stderr, "%d, ", _x); \
+    fprintf(stderr, "\n"); \
+  } while (0)
+  #define PRINTMAP(map) do { \
+    fprintf(stderr, "%s:  ", #map); \
+    for (const auto& _p : (map))  fprintf(stderr, "%d->%d ", _p.first, _p.second); \
+    fprintf(stderr, "\n"); \
+  } while (0)
+  #define DEBUGFOREACH(vec, stmt) do { \
+    fprintf(stderr, "%s:  ", #vec); \
+    for (const auto& x : (vec)) { \
+      stmt; \
+    } \
+  } while (0)
+#else
+  #define fprintf(...) // no-op
+  #define PRINTVEC(...) // no-op
+  #define PRINTMAP(...) // no-op
+#endif
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+void solve() {
+  long x, y;
+  cin >> x >> y;
+  if (x == y) {
+    cout << -1 << "\n";
+    return;
+  }
+  if (x < y)  swap(x, y);  // x > y
+
+  long k = (1LL << 50) - x;
+  fprintf(stderr, "x+k=%lld, y+k=%lld, sum=%lld, xor=%lld\n", x+k, y+k, x+k+y+k, (x+k)^(y+k));
+  cout << k << "\n";
+}
+
+
+
+
+
+
+
+
+
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  int t;
+  cin >> t;
+  while (t--)  solve();
+  return 0;
+}
