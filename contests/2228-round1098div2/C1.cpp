9b23c38eacef5c63c52971a13fa7a4fb32d071c2
greatericontop <greateric@protonmail.com>
2026-05-16 12:36:36 -0400

    s

--- contests/2228-round1098div2/C1.cpp
+++ contests/2228-round1098div2/C1.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 
-#define GREATERIC_DEBUG
+//#define GREATERIC_DEBUG
 
 
 #ifdef GREATERIC_DEBUG
@@ -60,12 +60,65 @@ template <typename It, typename Container> constexpr static inline int itertoi(c
 
 
 
-
+long calcnumformask(long mask, int msb, int digits[2]) {
+  long x = 0;
+  for (int bit = msb; bit >= 0; bit--) {
+    x *= 10;
+    x += digits[(mask >> bit) & 1];
+  }
+  return x;
+}
 
 void solve() {
-  int n;
-  cin >> n;
-  // -fsanitize=undefined -fsanitize=address -fno-sanitize-recover -Wall -Werror -Wextra -Wshadow -Wfloat-equal -Wno-error=unused-variable -Wno-error=unused-parameter -D_GLIBCXX_DEBUG -D_GLIBCXX_DEBUG_PEDANTIC -D_FORTIFY_SOURCE=2 -O1
+  long a; int n;
+  cin >> a >> n;
+  assert(n == 2);
+  int digits[2];
+  FORI(2)  cin >> digits[i];
+
+  int numdigitsina = 0;
+  long temp = a;
+  while (temp > 0) {
+    temp /= 10;
+    numdigitsina++;
+  }
+
+  // find first num >= a
+  long l = 0, r = (1<<numdigitsina)-1;
+  while (l < r) {
+    long mask = l + (r-l)/2;
+    long x = calcnumformask(mask, numdigitsina-1, digits);
+    fprintf(stderr, "number %lld for mask %lld\n", x, mask);
+    if (x < a) {
+      l = mask+1;
+    } else {
+      // x >= a
+      r = mask;
+    }
+  }
+
+  fprintf(stderr, "ended with masks %lld, %lld\n", l, l-1);
+  long x1 = calcnumformask(l-1, numdigitsina-1, digits);
+  long x2 = calcnumformask(l, numdigitsina-1, digits);
+  fprintf(stderr, "numbers %lld, %lld\n", x1, x2);
+
+  // x3: with one less digit
+  long x3 = 0;
+  for (int i = 0; i < numdigitsina-1; i++) {
+    x3 *= 10;
+    x3 += digits[1];
+  }
+  fprintf(stderr, "number with one less digit: %lld\n", x3);
+  long x4 = 0;
+  for (int i = 0; i < numdigitsina+1; i++) {
+    x4 *= 10;
+    x4 += (i == 0 && digits[0] == 0) ? digits[1] : digits[0];
+  }
+  fprintf(stderr, "number with one more digit: %lld\n", x4);
+
+  long ans = min(min(min(abs(x1-a), abs(x2-a)), abs(x3-a)), abs(x4-a));
+  cout << ans << "\n";
+
 
 }
 
