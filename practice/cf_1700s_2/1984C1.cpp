ee3477ff6cdafa725277cd5eab2c7158dbe50814
greatericontop <greateric@protonmail.com>
2026-05-19 13:09:34 -0400

    save

---
+++ practice/cf_1700s_2/1984C1.cpp
@@ -0,0 +1,98 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+#define GREATERIC_DEBUG
+
+
+#ifdef GREATERIC_DEBUG
+  #define PRINTVEC(vec) do { \
+    fprintf(stderr, "%s:  ", #vec); \
+    for (const auto& _x : (vec))  fprintf(stderr, "%d ", _x); \
+    fprintf(stderr, "\n"); \
+  } while (0)
+  #define PRINTVECL(vec) do { \
+    fprintf(stderr, "%s:  ", #vec); \
+    for (const auto& _x : (vec))  fprintf(stderr, "%lld ", _x); \
+    fprintf(stderr, "\n"); \
+  } while (0)
+  #define PRINTMAP(map) do { \
+    fprintf(stderr, "%s:   ", #map); \
+    for (const auto& _p : (map))  fprintf(stderr, "%d->%d  ", _p.first, _p.second); \
+    fprintf(stderr, "\n"); \
+  } while (0)
+  #define PRINTVECP(vec) do { \
+    fprintf(stderr, "%s:   ", #vec); \
+    for (const auto& _p : (vec))  fprintf(stderr, "[%d %d],  ", _p.first, _p.second); \
+    fprintf(stderr, "\n"); \
+  } while (0)
+  #define PRINTVECPL(vec) do { \
+    fprintf(stderr, "%s:   ", #vec); \
+    for (const auto& _p : (vec))  fprintf(stderr, "[%lld %lld],  ", _p.first, _p.second); \
+    fprintf(stderr, "\n"); \
+  } while (0)
+#else
+  #define fprintf(...)
+  #define PRINTVEC(...)
+  #define PRINTVECL(...)
+  #define PRINTMAP(...)
+  #define PRINTVECP(...)
+  #define PRINTVECPL(...)
+#endif
+#define long long long
+#define pb push_back
+#define LONG(x) ((long) (x))
+#define INT(x) ((int) (x))
+#define FORI(x) for (int i = 0; i < (x); i++)
+using pairii = pair<int, int>;
+using pairll = pair<long, long>;
+using AdjList = vector<vector<int>>;
+// Positive numbers only
+constexpr static inline int ceildiv(int a, int b) { return (a + b - 1) / b; }
+constexpr static inline long ceildivl(long a, long b) { return (a + b - 1) / b; }
+// Only O(1) for vectors!
+template <typename It, typename Container> constexpr static inline int itertoi(const It& it, const Container& container) { return distance(container.begin(), it); }
+
+
+
+
+
+
+
+
+
+
+void solve() {
+  int n;
+  cin >> n;
+  vector<long> a(n);
+  FORI(n)  cin >> a[i];
+
+  long mini = 0, maxi = 0;
+  for (int i = 0; i < n; i++) {
+    long new_mini = mini + a[i];
+    long new_maxi = max(abs(maxi + a[i]), abs(mini + a[i]));
+    mini = new_mini;
+    maxi = new_maxi;
+  }
+
+  cout << maxi << "\n";
+}
+
+
+
+
+
+
+
+
+
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  int t = 1;
+  cin >> t;
+  while (t--)  solve();
+  return 0;
+}
