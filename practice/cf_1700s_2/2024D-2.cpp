aaecb96ce9514e1e60d186726637b6565d1f76fe
greatericontop <greateric@protonmail.com>
2026-05-19 22:53:49 -0400

    save

---
+++ practice/cf_1700s_2/2024D-2.cpp
@@ -0,0 +1,171 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+#define GREATERIC_DEBUG
+
+
+#ifdef GREATERIC_DEBUG
+  #define PRINTVEC(vec) do { \
+    fprintf(stderr, "%s:  ", #vec); \
+    for (const auto& _x : (vec))  fprintf(stderr, "%d ", _x); \
+    fprintf(stderr, "\n"); \
+  } while (0)
+  #define PRINTVECL(vec) do { \
+    fprintf(stderr, "%s:  ", #vec); \
+    for (const auto& _x : (vec))  fprintf(stderr, "%lld ", _x); \
+    fprintf(stderr, "\n"); \
+  } while (0)
+  #define PRINTMAP(map) do { \
+    fprintf(stderr, "%s:   ", #map); \
+    for (const auto& _p : (map))  fprintf(stderr, "%d->%d  ", _p.first, _p.second); \
+    fprintf(stderr, "\n"); \
+  } while (0)
+  #define PRINTVECP(vec) do { \
+    fprintf(stderr, "%s:   ", #vec); \
+    for (const auto& _p : (vec))  fprintf(stderr, "[%d %d],  ", _p.first, _p.second); \
+    fprintf(stderr, "\n"); \
+  } while (0)
+  #define PRINTVECPL(vec) do { \
+    fprintf(stderr, "%s:   ", #vec); \
+    for (const auto& _p : (vec))  fprintf(stderr, "[%lld %lld],  ", _p.first, _p.second); \
+    fprintf(stderr, "\n"); \
+  } while (0)
+#else
+  #define fprintf(...)
+  #define PRINTVEC(...)
+  #define PRINTVECL(...)
+  #define PRINTMAP(...)
+  #define PRINTVECP(...)
+  #define PRINTVECPL(...)
+#endif
+#define long long long
+#define pb push_back
+#define LONG(x) ((long) (x))
+#define INT(x) ((int) (x))
+#define FORI(x) for (int i = 0; i < (x); i++)
+using pairii = pair<int, int>;
+using pairll = pair<long, long>;
+using AdjList = vector<vector<int>>;
+// Positive numbers only
+constexpr static inline int ceildiv(int a, int b) { return (a + b - 1) / b; }
+constexpr static inline long ceildivl(long a, long b) { return (a + b - 1) / b; }
+// Only O(1) for vectors!
+template <typename It, typename Container> constexpr static inline int itertoi(const It& it, const Container& container) { return distance(container.begin(), it); }
+
+/*
+ * Segment tree skeleton. Fill stuff in where appropriate.
+ * Handwritten by me (might be slightly slow).
+ */
+#define exp(x) (1 << (x))
+template <class T, class Combiner> class SegmentTree {
+private:
+  int max_layer;
+  int n;
+  vector<vector<T>> segments; // segments[l] contains 0 to 2^l - 1
+  Combiner combiner;
+  T combine_empty; // the identity/null element (e.g. 0 for sum, INT_MAX for min, etc.)
+
+public:
+  explicit SegmentTree(int max_layer, Combiner combiner, T combine_empty, const vector<T>& initialize)
+      : max_layer(max_layer), n(exp(max_layer)), segments(max_layer + 1),
+      combiner(combiner), combine_empty(combine_empty) {
+    assert(initialize.size() <= n);
+    segments[0] = initialize;
+    while (segments[0].size() < n)  segments[0].push_back(combine_empty);
+
+    for (int layer = 1; layer <= max_layer; layer++) {
+      for (int i = 0; i < exp(max_layer-layer); i++) {
+        segments[layer].push_back(combiner(segments[layer-1][2*i], segments[layer-1][2*i+1]));
+      }
+    }
+  }
+
+  void point_update(int i, T new_value) {
+    segments[0][i] = new_value;
+    for (int layer = 1; layer <= max_layer; layer++) {
+      i /= 2;
+      segments[layer][i] = combiner(segments[layer-1][2*i], segments[layer-1][2*i+1]);
+    }
+  }
+
+  /* Range query left to right inclusive, 0-indexed */
+  T range_query(int left, int right) {
+    T answer = combine_empty;
+    while (left <= right) {
+      // Find biggest interval (aka what's the biggest power of 2 that divides a)
+      int layer_i = left;
+      int layer = 0;
+      while ((left % (2 << layer) == 0) && (layer < 18) && (left + (2 << layer) - 1 <= right)) {
+        layer++;
+        layer_i /= 2;
+      }
+      answer = combiner(answer, segments[layer][layer_i]);
+      left += exp(layer);
+    }
+    return answer;
+  }
+};
+
+
+
+
+
+
+
+
+
+
+void solve() {
+  int n;
+  cin >> n;
+  vector<long> scores(n+1);
+  for (int i = 1; i <= n; i++)  cin >> scores[i];
+  vector<int> b(n+1, -1);
+  for (int i = 1; i <= n; i++)  cin >> b[i];
+
+  int logn = 1;
+  while (exp(logn) < n+100)  logn++;
+  auto min_combiner = [](const long& a, const long& b) { return min(a, b); };
+  vector<long> empty(n+1, LONG(1e18));
+  empty[1] = 0;
+  SegmentTree<long, decltype(min_combiner)> dp(logn, min_combiner, LONG(1e18), empty);
+
+  for (int i = 1; i <= n; i++) {
+    long cost_to_get_to_i = dp.range_query(i, n);
+    long cost_to_bi = cost_to_get_to_i + scores[i];
+    if (cost_to_bi < dp.range_query(b[i], b[i])) {
+      dp.point_update(b[i], cost_to_bi);
+    }
+  }
+
+  long acc = 0;
+  long ans = -1;
+  for (int i = 1; i <= n; i++) {
+    acc += scores[i];
+    // subtract cost it takes to get here
+    long ans_here = acc - dp.range_query(i, n);
+    ans = max(ans, ans_here);
+  }
+
+  cout << ans << "\n";
+
+}
+
+
+
+
+
+
+
+
+
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  int t = 1;
+  cin >> t;
+  while (t--)  solve();
+  return 0;
+}
---
+++ practice/cf_1700s_2/2024D.cpp
@@ -0,0 +1,163 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+#define GREATERIC_DEBUG
+
+
+#ifdef GREATERIC_DEBUG
+  #define PRINTVEC(vec) do { \
+    fprintf(stderr, "%s:  ", #vec); \
+    for (const auto& _x : (vec))  fprintf(stderr, "%d ", _x); \
+    fprintf(stderr, "\n"); \
+  } while (0)
+  #define PRINTVECL(vec) do { \
+    fprintf(stderr, "%s:  ", #vec); \
+    for (const auto& _x : (vec))  fprintf(stderr, "%lld ", _x); \
+    fprintf(stderr, "\n"); \
+  } while (0)
+  #define PRINTMAP(map) do { \
+    fprintf(stderr, "%s:   ", #map); \
+    for (const auto& _p : (map))  fprintf(stderr, "%d->%d  ", _p.first, _p.second); \
+    fprintf(stderr, "\n"); \
+  } while (0)
+  #define PRINTVECP(vec) do { \
+    fprintf(stderr, "%s:   ", #vec); \
+    for (const auto& _p : (vec))  fprintf(stderr, "[%d %d],  ", _p.first, _p.second); \
+    fprintf(stderr, "\n"); \
+  } while (0)
+  #define PRINTVECPL(vec) do { \
+    fprintf(stderr, "%s:   ", #vec); \
+    for (const auto& _p : (vec))  fprintf(stderr, "[%lld %lld],  ", _p.first, _p.second); \
+    fprintf(stderr, "\n"); \
+  } while (0)
+#else
+  #define fprintf(...)
+  #define PRINTVEC(...)
+  #define PRINTVECL(...)
+  #define PRINTMAP(...)
+  #define PRINTVECP(...)
+  #define PRINTVECPL(...)
+#endif
+#define long long long
+#define pb push_back
+#define LONG(x) ((long) (x))
+#define INT(x) ((int) (x))
+#define FORI(x) for (int i = 0; i < (x); i++)
+using pairii = pair<int, int>;
+using pairll = pair<long, long>;
+using AdjList = vector<vector<int>>;
+// Positive numbers only
+constexpr static inline int ceildiv(int a, int b) { return (a + b - 1) / b; }
+constexpr static inline long ceildivl(long a, long b) { return (a + b - 1) / b; }
+// Only O(1) for vectors!
+template <typename It, typename Container> constexpr static inline int itertoi(const It& it, const Container& container) { return distance(container.begin(), it); }
+
+/*
+ * Segment tree skeleton. Fill stuff in where appropriate.
+ * Handwritten by me (might be slightly slow).
+ */
+#define exp(x) (1 << (x))
+template <class T, class Combiner> class SegmentTree {
+private:
+  int max_layer;
+  int n;
+  vector<vector<T>> segments; // segments[l] contains 0 to 2^l - 1
+  Combiner combiner;
+  T combine_empty; // the identity/null element (e.g. 0 for sum, INT_MAX for min, etc.)
+
+public:
+  explicit SegmentTree(int max_layer, Combiner combiner, T combine_empty, const vector<T>& initialize)
+      : max_layer(max_layer), n(exp(max_layer)), segments(max_layer + 1),
+      combiner(combiner), combine_empty(combine_empty) {
+    assert(initialize.size() <= n);
+    segments[0] = initialize;
+    while (segments[0].size() < n)  segments[0].push_back(combine_empty);
+
+    for (int layer = 1; layer <= max_layer; layer++) {
+      for (int i = 0; i < exp(max_layer-layer); i++) {
+        segments[layer].push_back(combiner(segments[layer-1][2*i], segments[layer-1][2*i+1]));
+      }
+    }
+  }
+
+  void point_update(int i, T new_value) {
+    segments[0][i] = new_value;
+    for (int layer = 1; layer <= max_layer; layer++) {
+      i /= 2;
+      segments[layer][i] = combiner(segments[layer-1][2*i], segments[layer-1][2*i+1]);
+    }
+  }
+
+  /* Range query left to right inclusive, 0-indexed */
+  T range_query(int left, int right) {
+    T answer = combine_empty;
+    while (left <= right) {
+      // Find biggest interval (aka what's the biggest power of 2 that divides a)
+      int layer_i = left;
+      int layer = 0;
+      while ((left % (2 << layer) == 0) && (layer < 18) && (left + (2 << layer) - 1 <= right)) {
+        layer++;
+        layer_i /= 2;
+      }
+      answer = combiner(answer, segments[layer][layer_i]);
+      left += exp(layer);
+    }
+    return answer;
+  }
+};
+
+
+
+
+
+
+
+
+
+
+void solve() {
+  int n;
+  cin >> n;
+  vector<long> scores(n+1);
+  for (int i = 1; i <= n; i++)  cin >> scores[i];
+  vector<int> b(n+1, -1);
+  for (int i = 1; i <= n; i++)  cin >> b[i];
+
+  int logn = 1;
+  while (exp(logn) < n+100)  logn++;
+  auto max_combiner = [](const int& a, const int& b) { return max(a, b); };
+  SegmentTree<int, decltype(max_combiner)> segment_tree(logn, max_combiner, -1, b);
+
+  int max_i = 1;
+  while (true) {  //executes at most n times
+    int next_max_i = segment_tree.range_query(1, max_i);
+    if (next_max_i <= max_i)  break;
+    max_i = next_max_i;
+  }
+
+  long ans = 0;
+  for (int i = 1; i <= max_i; i++) {
+    ans += scores[i];
+  }
+  cout << ans << "\n";
+
+}
+
+
+
+
+
+
+
+
+
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  int t = 1;
+  cin >> t;
+  while (t--)  solve();
+  return 0;
+}
