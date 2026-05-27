d6a18922cf172a91c3217ab0734bce71689207b0
greatericontop <greateric@protonmail.com>
2026-05-27 14:50:26 -0400

    save

---
+++ practice/cf_1700s_2/2202D.cpp
@@ -0,0 +1,125 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+#define GREATERIC_DEBUG
+
+
+#ifdef GREATERIC_DEBUG
+  #define PRINTVEC(vec) do { \
+    fprintf(stderr, "%s:  ", #vec); \
+    for (const auto& _x : (vec))  fprintf(stderr, "%d ", _x); \
+    fprintf(stderr, "\n"); \
+  } while (0)
+  #define PRINTVECL(vec) do { \
+    fprintf(stderr, "%s:  ", #vec); \
+    for (const auto& _x : (vec))  fprintf(stderr, "%lld ", _x); \
+    fprintf(stderr, "\n"); \
+  } while (0)
+  #define PRINTMAP(map) do { \
+    fprintf(stderr, "%s:   ", #map); \
+    for (const auto& _p : (map))  fprintf(stderr, "%d->%d  ", _p.first, _p.second); \
+    fprintf(stderr, "\n"); \
+  } while (0)
+  #define PRINTVECP(vec) do { \
+    fprintf(stderr, "%s:   ", #vec); \
+    for (const auto& _p : (vec))  fprintf(stderr, "[%d %d],  ", _p.first, _p.second); \
+    fprintf(stderr, "\n"); \
+  } while (0)
+  #define PRINTVECPL(vec) do { \
+    fprintf(stderr, "%s:   ", #vec); \
+    for (const auto& _p : (vec))  fprintf(stderr, "[%lld %lld],  ", _p.first, _p.second); \
+    fprintf(stderr, "\n"); \
+  } while (0)
+#else
+  #define fprintf(...)
+  #define PRINTVEC(...)
+  #define PRINTVECL(...)
+  #define PRINTMAP(...)
+  #define PRINTVECP(...)
+  #define PRINTVECPL(...)
+#endif
+#define long long long
+#define pb push_back
+#define LONG(x) ((long) (x))
+#define INT(x) ((int) (x))
+#define FORI(x) for (int i = 0; i < (x); i++)
+using pairii = pair<int, int>;
+using pairll = pair<long, long>;
+using AdjList = vector<vector<int>>;
+// Positive numbers only
+constexpr static inline int ceildiv(int a, int b) { return (a + b - 1) / b; }
+constexpr static inline long ceildivl(long a, long b) { return (a + b - 1) / b; }
+// Only O(1) for vectors!
+template <typename It, typename Container> constexpr static inline int itertoi(const It& it, const Container& container) { return distance(container.begin(), it); }
+
+
+
+
+
+
+
+
+
+
+void solve() {
+  int n, k;
+  cin >> n >> k;
+
+  if (n == 1) {
+    if (k == 1) {
+      cout << "YES\n";
+      cout << "1 1\n";
+    } else {
+      cout << "NO\n";
+    }
+  } else if (n == k) {
+    cout << "YES\n";
+    for (int i = 1; i <= n; i++) {
+      cout << i << " " << i << " ";
+    }
+    cout << "\n";
+  } else {
+    int min_k = n;
+    int max_k = n + ceildiv(n, 2);
+    if (k < min_k || k > max_k) {
+      cout << "NO\n";
+      return;
+    }
+    int extra = k - n;
+    int n_new = min(n, extra*2);
+    assert(ceildiv(n_new, 2) == extra);
+
+    cout << "YES\n";
+    for (int i = n_new+1; i <= n; i++) {
+      cout << i << " " << i << " ";
+    }
+    for (int i = 1; i <= n_new; i++) {
+      cout << i << " ";
+    }
+    for (int i = 1; i <= n_new; i++) {
+      cout << i << " ";
+    }
+    cout << "\n";
+
+  }
+
+}
+
+
+
+
+
+
+
+
+
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  int t = 1;
+  cin >> t;
+  while (t--)  solve();
+  return 0;
+}
