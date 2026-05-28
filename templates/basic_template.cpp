6db2d0fb2fd470f851b15dbd19215eb4f3ea10d8
greatericontop <greateric@protonmail.com>
2026-05-27 20:47:24 -0400

    update template

--- templates/basic_template.cpp
+++ templates/basic_template.cpp
@@ -44,14 +44,15 @@ using namespace std;
 #define LONG(x) ((long) (x))
 #define INT(x) ((int) (x))
 #define FORI(x) for (int i = 0; i < (x); i++)
+#define FORI1(x) for (int i = 1; i <= (x); i++)
 using pairii = pair<int, int>;
 using pairll = pair<long, long>;
 using AdjList = vector<vector<int>>;
 // Positive numbers only
 constexpr static inline int ceildiv(int a, int b) { return (a + b - 1) / b; }
 constexpr static inline long ceildivl(long a, long b) { return (a + b - 1) / b; }
-// Only O(1) for vectors!
-template <typename It, typename Container> constexpr static inline int itertoi(const It& it, const Container& container) { return distance(container.begin(), it); }
+//constexpr static long MOD = 1'000'000'007LL;
+//constexpr static long MOD =   998'244'353LL;
 
 
 
@@ -65,7 +66,6 @@ template <typename It, typename Container> constexpr static inline int itertoi(c
 void solve() {
   int n;
   cin >> n;
-  // -fsanitize=undefined -fsanitize=address -fno-sanitize-recover -Wall -Werror -Wextra -Wshadow -Wfloat-equal -Wno-error=unused-variable -Wno-error=unused-parameter -D_GLIBCXX_DEBUG -D_GLIBCXX_DEBUG_PEDANTIC -D_FORTIFY_SOURCE=2 -O1
 
 }
 
@@ -86,3 +86,6 @@ int main() {
   while (t--)  solve();
   return 0;
 }
+
+/*  -fsanitize=undefined -fsanitize=address -fno-sanitize-recover -Wall -Werror -Wextra -Wshadow -Wfloat-equal
+    -Wno-error=unused-variable -Wno-error=unused-parameter -D_GLIBCXX_DEBUG -D_GLIBCXX_DEBUG_PEDANTIC -D_FORTIFY_SOURCE=2 -O1  */
