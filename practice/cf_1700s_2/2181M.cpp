23f26686311c87bd3ad30164d3d6cd4593497224
greatericontop <greateric@protonmail.com>
2026-05-28 14:19:09 -0400

    save

---
+++ practice/cf_1700s_2/2181M.cpp
@@ -0,0 +1,137 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+#define GREATERIC_DEBUG
+
+
+#ifdef GREATERIC_DEBUG
+  #define PRINTVEC(vec) do { \
+    fprintf(stderr, "%s:  ", #vec); \
+    for (const auto& _x : (vec))  fprintf(stderr, "%d ", _x); \
+    fprintf(stderr, "\n"); \
+  } while (0)
+  #define PRINTVECL(vec) do { \
+    fprintf(stderr, "%s:  ", #vec); \
+    for (const auto& _x : (vec))  fprintf(stderr, "%lld ", _x); \
+    fprintf(stderr, "\n"); \
+  } while (0)
+  #define PRINTMAP(map) do { \
+    fprintf(stderr, "%s:   ", #map); \
+    for (const auto& _p : (map))  fprintf(stderr, "%d->%d  ", _p.first, _p.second); \
+    fprintf(stderr, "\n"); \
+  } while (0)
+  #define PRINTVECP(vec) do { \
+    fprintf(stderr, "%s:   ", #vec); \
+    for (const auto& _p : (vec))  fprintf(stderr, "[%d %d],  ", _p.first, _p.second); \
+    fprintf(stderr, "\n"); \
+  } while (0)
+  #define PRINTVECPL(vec) do { \
+    fprintf(stderr, "%s:   ", #vec); \
+    for (const auto& _p : (vec))  fprintf(stderr, "[%lld %lld],  ", _p.first, _p.second); \
+    fprintf(stderr, "\n"); \
+  } while (0)
+#else
+  #define fprintf(...)
+  #define PRINTVEC(...)
+  #define PRINTVECL(...)
+  #define PRINTMAP(...)
+  #define PRINTVECP(...)
+  #define PRINTVECPL(...)
+#endif
+#define long long long
+#define pb push_back
+#define LONG(x) ((long) (x))
+#define INT(x) ((int) (x))
+#define FORI(x) for (int i = 0; i < (x); i++)
+#define FORI1(x) for (int i = 1; i <= (x); i++)
+using pairii = pair<int, int>;
+using pairll = pair<long, long>;
+using AdjList = vector<vector<int>>;
+// Positive numbers only
+constexpr static inline int ceildiv(int a, int b) { return (a + b - 1) / b; }
+constexpr static inline long ceildivl(long a, long b) { return (a + b - 1) / b; }
+//constexpr static long MOD = 1'000'000'007LL;
+//constexpr static long MOD =   998'244'353LL;
+
+
+
+
+
+
+
+
+
+constexpr static long INF = LONG(1e18);
+
+
+void solve() {
+  string xs, ys;
+  cin >> xs >> ys;
+  int n = xs.size();
+  vector<bool> x(n), y(n);
+  FORI(n)  x[i] = (xs[i] == '1');
+  FORI(n)  y[i] = (ys[i] == '1');
+
+  // dp[i][a][b] = min cost to satisfy up to i with x[i]=a, y[i]=b
+  vector<long[2][2]> dp(n);
+  dp[0][0][0] = (x[0] == 1) + (y[0] == 1);
+  dp[0][0][1] = INF;
+  dp[0][1][0] = INF;
+  dp[0][1][1] = (x[0] == 0) + (y[0] == 0);
+  for (int i = 1; i < n; i++) {
+    dp[i][0][0] = INF;
+    dp[i][0][1] = INF;
+    dp[i][1][0] = INF;
+    dp[i][1][1] = INF;
+  }
+
+  for (int i = 1; i < n; i++) {
+    // transition from dp[i-1][a][b] -> dp[i][c][d]
+    for (int a = 0; a < 2; a++) {
+      for (int b = 0; b < 2; b++) {
+        for (int c = 0; c < 2; c++) {
+          for (int d = 0; d < 2; d++) {
+            // Check that the combination is valid
+            int expected_value_of_c = (b ^ d);  //if parity bit changed, then c must be 1
+            if (expected_value_of_c != c)  continue;
+
+            long cost = dp[i-1][a][b] + (x[i] != c) + (y[i] != d);
+            dp[i][c][d] = min(dp[i][c][d], cost);
+          }
+        }
+      }
+    }
+  }
+
+  long ans = INF;
+  for (int a = 0; a < 2; a++) {
+    for (int b = 0; b < 2; b++) {
+      ans = min(ans, dp[n-1][a][b]);
+    }
+  }
+  assert(ans != INF);
+  cout << ans << "\n";
+
+}
+
+
+
+
+
+
+
+
+
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  int t = 1;
+  cin >> t;
+  while (t--)  solve();
+  return 0;
+}
+
+/*  -fsanitize=undefined -fsanitize=address -fno-sanitize-recover -Wall -Werror -Wextra -Wshadow -Wfloat-equal
+    -Wno-error=unused-variable -Wno-error=unused-parameter -D_GLIBCXX_DEBUG -D_GLIBCXX_DEBUG_PEDANTIC -D_FORTIFY_SOURCE=2 -O1  */
