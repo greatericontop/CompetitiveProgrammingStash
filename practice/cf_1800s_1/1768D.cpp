f9b9af5051d55fe72aff6840d28bb58b647dafec
greatericontop <greateric@protonmail.com>
2026-06-06 20:44:31 -0400

    s

---
+++ practice/cf_1800s_1/1768D.cpp
@@ -0,0 +1,126 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+//#define GREATERIC_DEBUG
+
+
+#ifdef GREATERIC_DEBUG
+  #define PRINTVEC(vec) do { \
+    fprintf(stderr, "%s:  ", #vec); \
+    for (const auto& _x : (vec))  fprintf(stderr, "%d ", _x); \
+    fprintf(stderr, "\n"); \
+  } while (0)
+  #define PRINTVECL(vec) do { \
+    fprintf(stderr, "%s:  ", #vec); \
+    for (const auto& _x : (vec))  fprintf(stderr, "%lld ", _x); \
+    fprintf(stderr, "\n"); \
+  } while (0)
+  #define PRINTMAP(map) do { \
+    fprintf(stderr, "%s:   ", #map); \
+    for (const auto& _p : (map))  fprintf(stderr, "%d->%d  ", _p.first, _p.second); \
+    fprintf(stderr, "\n"); \
+  } while (0)
+  #define PRINTVECP(vec) do { \
+    fprintf(stderr, "%s:   ", #vec); \
+    for (const auto& _p : (vec))  fprintf(stderr, "[%d %d],  ", _p.first, _p.second); \
+    fprintf(stderr, "\n"); \
+  } while (0)
+  #define PRINTVECPL(vec) do { \
+    fprintf(stderr, "%s:   ", #vec); \
+    for (const auto& _p : (vec))  fprintf(stderr, "[%lld %lld],  ", _p.first, _p.second); \
+    fprintf(stderr, "\n"); \
+  } while (0)
+#else
+  #define fprintf(...)
+  #define PRINTVEC(...)
+  #define PRINTVECL(...)
+  #define PRINTMAP(...)
+  #define PRINTVECP(...)
+  #define PRINTVECPL(...)
+#endif
+#define long long long
+#define pb push_back
+#define LONG(x) ((long) (x))
+#define INT(x) ((int) (x))
+#define FORI(x) for (int i = 0; i < (x); i++)
+#define FORI1(x) for (int i = 1; i <= (x); i++)
+using pairii = pair<int, int>;
+using pairll = pair<long, long>;
+using AdjList = vector<vector<int>>;
+// Positive numbers only
+constexpr static inline int ceildiv(int a, int b) { return (a + b - 1) / b; }
+constexpr static inline long ceildivl(long a, long b) { return (a + b - 1) / b; }
+//constexpr static long MOD = 1'000'000'007LL;
+//constexpr static long MOD =   998'244'353LL;
+
+
+
+
+
+
+
+
+
+
+void solve() {
+  int n;
+  cin >> n;
+  vector<int> p(n+1);
+  FORI1(n)  cin >> p[i];
+
+  vector<int> cycle_ids(n+1, -1);  //-1 meaning unvisited
+  for (int start = 1; start <= n; start++) {
+    if (cycle_ids[start] != -1)  continue;
+    int cycle_id = start;
+    int cur = start;
+    while (true) {
+      cycle_ids[cur] = cycle_id;
+      cur = p[cur];
+      if (cycle_ids[cur] != -1)  break;
+    }
+  }
+
+  set<int> cycle_ids_set;
+  for (int i = 1; i <= n; i++)  cycle_ids_set.insert(cycle_ids[i]);
+  int ans_for_sorted = n - INT(cycle_ids_set.size());
+  fprintf(stderr, "ans for sorted: %d\n", ans_for_sorted);
+
+  int best = INT_MAX;
+  for (int i = 1; i < n; i++) {
+    // try swapping i and i+1
+    if (cycle_ids[i] == cycle_ids[i+1]) {
+      // they will now be in different cycles, so the answer is now better
+      best = min(best, ans_for_sorted - 1);
+    } else {
+      // they will now be in the same cycle, so the answer is now worse
+      best = min(best, ans_for_sorted + 1);
+    }
+  }
+
+  assert(best != INT_MAX);
+
+  cout << best << "\n";
+
+}
+
+
+
+
+
+
+
+
+
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  int t = 1;
+  cin >> t;
+  while (t--)  solve();
+  return 0;
+}
+
+/*  -fsanitize=undefined -fsanitize=address -fno-sanitize-recover -Wall -Werror -Wextra -Wshadow -Wfloat-equal
+    -Wno-error=unused-variable -Wno-error=unused-parameter -D_GLIBCXX_DEBUG -D_GLIBCXX_DEBUG_PEDANTIC -D_FORTIFY_SOURCE=2 -O1  */
