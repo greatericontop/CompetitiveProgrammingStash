3410f541a298973dc76ec31ed2775a7289d8e213
greatericontop <greateric@protonmail.com>
2026-06-14 18:23:39 -0400

    save

--- practice/cf_1800s_1/1930D2-golf-py.py
+++ practice/cf_1800s_1/1930D2-golf-py.py
@@ -3,5 +3,5 @@ I=input
 i=int
 for x in r(i(I())):
  n=i(I());s=I();d=[0]*(n+5)
- for j in r(n-1,-1,-1):d[j]=d[j+1]if s[j]=='0'else n-j+d[j+3]
+ for j in r(n):d[j]=d[j-1]if s[n-1-j]=='0'else j+1+d[j-3]
  print(sum(d))
