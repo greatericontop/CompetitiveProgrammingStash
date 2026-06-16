2259aab7e76abcf5cf7b9a55c5771587efca153c
greatericontop <greateric@protonmail.com>
2026-06-16 19:36:05 -0400

    save

---
+++ practice/cf_2200s/1972D2-3.cpp
@@ -0,0 +1,136 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+#define GREATERIC_DEBUG
+
+
+#ifdef GREATERIC_DEBUG
+  #define PRINTVEC(vec) do { \
+    fprintf(stderr, "%s:  ", #vec); \
+    for (const auto& _x : (vec))  fprintf(stderr, "%d ", _x); \
+    fprintf(stderr, "\n"); \
+  } while (0)
+  #define PRINTVECL(vec) do { \
+    fprintf(stderr, "%s:  ", #vec); \
+    for (const auto& _x : (vec))  fprintf(stderr, "%lld ", _x); \
+    fprintf(stderr, "\n"); \
+  } while (0)
+  #define PRINTMAP(map) do { \
+    fprintf(stderr, "%s:   ", #map); \
+    for (const auto& _p : (map))  fprintf(stderr, "%d->%d  ", _p.first, _p.second); \
+    fprintf(stderr, "\n"); \
+  } while (0)
+  #define PRINTVECP(vec) do { \
+    fprintf(stderr, "%s:   ", #vec); \
+    for (const auto& _p : (vec))  fprintf(stderr, "[%d %d],  ", _p.first, _p.second); \
+    fprintf(stderr, "\n"); \
+  } while (0)
+  #define PRINTVECPL(vec) do { \
+    fprintf(stderr, "%s:   ", #vec); \
+    for (const auto& _p : (vec))  fprintf(stderr, "[%lld %lld],  ", _p.first, _p.second); \
+    fprintf(stderr, "\n"); \
+  } while (0)
+#else
+  #define fprintf(...)
+  #define PRINTVEC(...)
+  #define PRINTVECL(...)
+  #define PRINTMAP(...)
+  #define PRINTVECP(...)
+  #define PRINTVECPL(...)
+#endif
+#define long long long
+#define pb push_back
+#define LONG(x) ((long) (x))
+#define INT(x) ((int) (x))
+#define FORI(x) for (int i = 0; i < (x); i++)
+#define FORI1(x) for (int i = 1; i <= (x); i++)
+using pairii = pair<int, int>;
+using pairll = pair<long, long>;
+using AdjList = vector<vector<int>>;
+// Positive numbers only
+constexpr static inline int ceildiv(int a, int b) { return (a + b - 1) / b; }
+constexpr static inline long ceildivl(long a, long b) { return (a + b - 1) / b; }
+//constexpr static long MOD = 1'000'000'007LL;
+//constexpr static long MOD =   998'244'353LL;
+
+
+
+
+
+
+
+
+
+
+void solve(const vector<vector<int>>& factors) {
+  int n, m;
+  cin >> n >> m;
+
+  long ans = 0;
+
+  int gmax = min(n, m);
+  for (int g = 1; g <= gmax; g++) {
+    fprintf(stderr, "g = %d\n", g);
+    int xmax = n / g;
+    int ymax = m / g;
+    for (int f : factors[g]) {
+      fprintf(stderr, "  f = %d\n", f);
+      // x = g/f - y
+      // if g/f is too large
+      int gf = g/f;
+      int min_y_here = max(1, gf - xmax);
+      int max_y_here = min(ymax, gf - 1);
+      if (min_y_here <= max_y_here) {
+        fprintf(stderr, "    y in [%d, %d]  (+%d)\n", min_y_here, max_y_here, max_y_here - min_y_here + 1);
+        ans += max_y_here - min_y_here + 1;
+      }
+    }
+  }
+
+  //manually subtract out overcounts
+  for (int x = 1; x <= n; x++) {
+    for (int y = x; y <= m; y += x) {
+      int how_many_g = min(n/x, m/y);
+      ans -= how_many_g;
+    }
+  }
+  for (int y = 1; y <= m; y++) {
+    for (int x = 2*y; x <= n; x += y) {
+      int how_many_g = min(n/x, m/y);
+      ans -= how_many_g;
+    }
+  }
+
+  cout << ans << "\n";
+
+}
+
+
+
+
+
+
+
+
+
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  vector<vector<int>> factors(2'000'001);
+  for (int x = 1; x <= 2'000'000; x++) {
+    for (int y = x; y <= 2'000'000; y += x) {
+      factors[y].pb(x);
+    }
+  }
+
+  int t = 1;
+  cin >> t;
+  while (t--)  solve(factors);
+  return 0;
+}
+
+/*  -fsanitize=undefined -fsanitize=address -fno-sanitize-recover -Wall -Werror -Wextra -Wshadow -Wfloat-equal
+    -Wno-error=unused-variable -Wno-error=unused-parameter -D_GLIBCXX_DEBUG -D_GLIBCXX_DEBUG_PEDANTIC -D_FORTIFY_SOURCE=2 -O1  */
