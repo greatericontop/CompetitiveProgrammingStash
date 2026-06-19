46449160edb18c397b96e0d17dd85dfc57e2c116
greatericontop <greateric@protonmail.com>
2026-06-18 22:56:53 -0400

    s

---
+++ practice/cf_1800s_1/1987D.cpp
@@ -0,0 +1,136 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+#define GREATERIC_DEBUG
+
+
+#ifdef GREATERIC_DEBUG
+  #define PRINTVEC(vec) do { \
+    fprintf(stderr, "%s:  ", #vec); \
+    for (const auto& _x : (vec))  fprintf(stderr, "%d ", _x); \
+    fprintf(stderr, "\n"); \
+  } while (0)
+  #define PRINTVECL(vec) do { \
+    fprintf(stderr, "%s:  ", #vec); \
+    for (const auto& _x : (vec))  fprintf(stderr, "%lld ", _x); \
+    fprintf(stderr, "\n"); \
+  } while (0)
+  #define PRINTMAP(map) do { \
+    fprintf(stderr, "%s:   ", #map); \
+    for (const auto& _p : (map))  fprintf(stderr, "%d->%d  ", _p.first, _p.second); \
+    fprintf(stderr, "\n"); \
+  } while (0)
+  #define PRINTVECP(vec) do { \
+    fprintf(stderr, "%s:   ", #vec); \
+    for (const auto& _p : (vec))  fprintf(stderr, "[%d %d],  ", _p.first, _p.second); \
+    fprintf(stderr, "\n"); \
+  } while (0)
+  #define PRINTVECPL(vec) do { \
+    fprintf(stderr, "%s:   ", #vec); \
+    for (const auto& _p : (vec))  fprintf(stderr, "[%lld %lld],  ", _p.first, _p.second); \
+    fprintf(stderr, "\n"); \
+  } while (0)
+#else
+  #define fprintf(...)
+  #define PRINTVEC(...)
+  #define PRINTVECL(...)
+  #define PRINTMAP(...)
+  #define PRINTVECP(...)
+  #define PRINTVECPL(...)
+#endif
+#define long long long
+#define pb push_back
+#define LONG(x) ((long) (x))
+#define INT(x) ((int) (x))
+#define FORI(x) for (int i = 0; i < (x); i++)
+#define FORI1(x) for (int i = 1; i <= (x); i++)
+using pairii = pair<int, int>;
+using pairll = pair<long, long>;
+using AdjList = vector<vector<int>>;
+// Positive numbers only
+constexpr static inline int ceildiv(int a, int b) { return (a + b - 1) / b; }
+constexpr static inline long ceildivl(long a, long b) { return (a + b - 1) / b; }
+//constexpr static long MOD = 1'000'000'007LL;
+//constexpr static long MOD =   998'244'353LL;
+
+
+
+
+
+
+
+
+struct Entry {
+  int i;
+  int x;
+
+  bool operator < (const Entry& other) const {
+    if (x != other.x)  return x < other.x;
+    return i < other.i;
+  }
+};
+
+
+void solve() {
+  int n;
+  cin >> n;
+  vector<int> b(n);
+  FORI(n)  cin >> b[i];
+  map<int, int> freq;
+  FORI(n)  freq[b[i]]++;
+
+  vector<int> a;
+  for (const auto& p : freq) {
+    a.pb(p.second);
+  }
+  int m = a.size();
+
+  vector<Entry> entries(m);
+  FORI(m)  entries[i] = {i, a[i]};
+  sort(entries.begin(), entries.end());
+
+  vector<bool> taken(m, false);
+
+  for (Entry e : entries) {
+    // try
+    taken[e.i] = true;
+
+    bool ok = true;
+    int sum_a = 0, sum_b = 0;
+    for (int i = 0; i < m; i++) {
+      if (!taken[i])  sum_a++;
+      else  sum_b += a[i];
+      if (sum_b > sum_a) {
+        ok = false;
+        break;
+      }
+    }
+
+    if (!ok)  taken[e.i] = false;
+  }
+
+  cout << m - count(taken.begin(), taken.end(), true) << "\n";
+
+}
+
+
+
+
+
+
+
+
+
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  int t = 1;
+  cin >> t;
+  while (t--)  solve();
+  return 0;
+}
+
+/*  -fsanitize=undefined -fsanitize=address -fno-sanitize-recover -Wall -Werror -Wextra -Wshadow -Wfloat-equal
+    -Wno-error=unused-variable -Wno-error=unused-parameter -D_GLIBCXX_DEBUG -D_GLIBCXX_DEBUG_PEDANTIC -D_FORTIFY_SOURCE=2 -O1  */
