9d862c9f460fb6d13425212b67ffd114aec1e448
greatericontop <greateric@protonmail.com>
2026-08-18 23:16:28 -0400

    save

---
+++ practice/cf_1900s_2/2064D.cpp
@@ -0,0 +1,170 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+#define GREATERIC_DEBUG
+
+
+#ifdef GREATERIC_DEBUG
+  #define PRINTVEC(vec) do { \
+    fprintf(stderr, "%s:  ", #vec); \
+    for (const auto& _x : (vec))  fprintf(stderr, "%d ", _x); \
+    fprintf(stderr, "\n"); \
+  } while (0)
+  #define PRINTVECL(vec) do { \
+    fprintf(stderr, "%s:  ", #vec); \
+    for (const auto& _x : (vec))  fprintf(stderr, "%lld ", _x); \
+    fprintf(stderr, "\n"); \
+  } while (0)
+  #define PRINTMAP(map) do { \
+    fprintf(stderr, "%s:   ", #map); \
+    for (const auto& _p : (map))  fprintf(stderr, "%d->%d  ", _p.first, _p.second); \
+    fprintf(stderr, "\n"); \
+  } while (0)
+  #define PRINTVECP(vec) do { \
+    fprintf(stderr, "%s:   ", #vec); \
+    for (const auto& _p : (vec))  fprintf(stderr, "[%d %d],  ", _p.first, _p.second); \
+    fprintf(stderr, "\n"); \
+  } while (0)
+  #define PRINTVECPL(vec) do { \
+    fprintf(stderr, "%s:   ", #vec); \
+    for (const auto& _p : (vec))  fprintf(stderr, "[%lld %lld],  ", _p.first, _p.second); \
+    fprintf(stderr, "\n"); \
+  } while (0)
+  #define PRINTVECB(vec) do { \
+    fprintf(stderr, "%s:   ", #vec); \
+    for (const auto& _p : (vec))  fprintf(stderr, "%s,  ", _p ? "true" : "false"); \
+    fprintf(stderr, "\n"); \
+  } while (0)
+#else
+  #define fprintf(...)
+  #define PRINTVEC(...)
+  #define PRINTVECL(...)
+  #define PRINTMAP(...)
+  #define PRINTVECP(...)
+  #define PRINTVECPL(...)
+  #define PRINTVECB(...)
+#endif
+#define long int64_t
+#define pb push_back
+#define LONG(x) ((long) (x))
+#define INT(x) ((int) (x))
+#define FORI(x) for (int i = 0; i < (x); i++)
+#define FORI1(x) for (int i = 1; i <= (x); i++)
+using pairii = pair<int, int>;
+using pairll = pair<long, long>;
+using AdjList = vector<vector<int>>;
+// Positive numbers only
+constexpr static inline int ceildiv(int a, int b) { return (a + b - 1) / b; }
+constexpr static inline long ceildivl(long a, long b) { return (a + b - 1) / b; }
+// Round :a: down or up to the closest multiple of :b:
+constexpr static inline int rounddown(int a, int b) { return (a / b) * b; }
+constexpr static inline int roundup(int a, int b) { return ceildiv(a, b) * b; }
+//constexpr static long MOD = 1'000'000'007LL;
+//constexpr static long MOD =   998'244'353LL;
+
+
+
+
+
+
+
+
+struct Node {
+  vector<int> ids;
+  int next[2];
+};
+Node node_empty() {
+  return Node{vector<int>(), {-1, -1}};
+}
+
+
+void cull(vector<Node>& trie, int cur, vector<int>& answers, int answer) {
+  if (trie[cur].next[0] != -1)  cull(trie, trie[cur].next[0], answers, answer);
+  if (trie[cur].next[1] != -1)  cull(trie, trie[cur].next[1], answers, answer);
+  for (int id : trie[cur].ids)  answers[id] = answer;
+  trie[cur].next[0] = -1;
+  trie[cur].next[1] = -1;
+}
+
+
+void solve() {
+  int n, q;
+  cin >> n >> q;
+  vector<int> w(n);
+  FORI(n)  cin >> w[i];
+  vector<int> queries(q);
+  FORI(q)  cin >> queries[i];
+
+  vector<Node> trie;  trie.reserve(30*n + 1000);
+  trie.pb(node_empty());
+  for (int i = 0; i < q; i++) {
+    int cur = 0;
+    for (int b = 29; b >= 0; b--) {
+      int bit = (queries[i] >> b) & 1;
+      if (trie[cur].next[bit] == -1) {
+        trie[cur].next[bit] = INT(trie.size());
+        trie.pb(node_empty());
+      }
+      cur = trie[cur].next[bit];
+    }
+    trie[cur].ids.pb(i);
+  }
+
+  int accumulated_xor = 0;
+  vector<int> answers(q, n);
+  for (int i = n-1; i >= 0; i--) {
+    int cur = 0;
+    for (int b = 29; b >= 0; b--) {
+      int bit = (w[i] >> b) & 1;
+      int xor_accumulated = (accumulated_xor >> b) & 1;
+      if (bit == 1) {
+        // then we need to cull the 0 branch
+        if (trie[cur].next[xor_accumulated] != -1) {
+          cull(trie, trie[cur].next[xor_accumulated], answers, n-1-i);
+        }
+      }
+      cur = trie[cur].next[bit ^ xor_accumulated];  //then we'll go down that branch where there could be more divisions
+      if (cur == -1)  break;
+    }
+
+    accumulated_xor ^= w[i];
+  }
+
+  for (int i = 0; i < q; i++) {
+    cout << answers[i] << " ";
+  }
+  cout << "\n";
+
+
+}
+
+
+
+
+
+
+
+
+
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  int t = 1;
+  cin >> t;
+  while (t--)  solve();
+  return 0;
+}
+
+/*  -fsanitize=undefined -fsanitize=address -fno-sanitize-recover -Wall -Werror -Wextra -Wshadow -Wfloat-equal
+    -Wno-error=unused-variable -Wno-error=unused-parameter -D_GLIBCXX_DEBUG -D_GLIBCXX_DEBUG_PEDANTIC -D_FORTIFY_SOURCE=2 -O1  */
+
+/*
+ * This code contains the use of comments! You can identify them with the "//" or "/*" symbols.
+ * Comments are used to explain the code and make it easier to understand.
+ * They are ignored by the compiler and do not affect the execution of the program.
+ * In this code, comments are used to explain the purpose of the code, the input and output format, and the logic behind the solution.
+ * Unlike the 3 lines shown above, the comments in this code were lovingly hand-inserted and not a result of AI generated text.
+ * Thanks to sc3developer <3 for inspiring this message and for being a great mentor.
+ */
