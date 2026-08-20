0be2f7a5a53de5a7e96046d07f9d52a8e141ab06
greatericontop <greateric@protonmail.com>
2026-08-20 10:18:37 -0400

    save

---
+++ practice/cf_1900s_3/2165B.cpp
@@ -0,0 +1,160 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+#define GREATERIC_DEBUG
+
+
+#ifdef GREATERIC_DEBUG
+  #define PRINTVEC(vec) do { \
+    fprintf(stderr, "%s:  ", #vec); \
+    for (const auto& _x : (vec))  fprintf(stderr, "%d ", _x); \
+    fprintf(stderr, "\n"); \
+  } while (0)
+  #define PRINTVECL(vec) do { \
+    fprintf(stderr, "%s:  ", #vec); \
+    for (const auto& _x : (vec))  fprintf(stderr, "%lld ", _x); \
+    fprintf(stderr, "\n"); \
+  } while (0)
+  #define PRINTMAP(map) do { \
+    fprintf(stderr, "%s:   ", #map); \
+    for (const auto& _p : (map))  fprintf(stderr, "%d->%d  ", _p.first, _p.second); \
+    fprintf(stderr, "\n"); \
+  } while (0)
+  #define PRINTVECP(vec) do { \
+    fprintf(stderr, "%s:   ", #vec); \
+    for (const auto& _p : (vec))  fprintf(stderr, "[%d %d],  ", _p.first, _p.second); \
+    fprintf(stderr, "\n"); \
+  } while (0)
+  #define PRINTVECPL(vec) do { \
+    fprintf(stderr, "%s:   ", #vec); \
+    for (const auto& _p : (vec))  fprintf(stderr, "[%lld %lld],  ", _p.first, _p.second); \
+    fprintf(stderr, "\n"); \
+  } while (0)
+  #define PRINTVECB(vec) do { \
+    fprintf(stderr, "%s:   ", #vec); \
+    for (const auto& _p : (vec))  fprintf(stderr, "%s,  ", _p ? "true" : "false"); \
+    fprintf(stderr, "\n"); \
+  } while (0)
+#else
+  #define fprintf(...)
+  #define PRINTVEC(...)
+  #define PRINTVECL(...)
+  #define PRINTMAP(...)
+  #define PRINTVECP(...)
+  #define PRINTVECPL(...)
+  #define PRINTVECB(...)
+#endif
+#define long int64_t
+#define pb push_back
+#define LONG(x) ((long) (x))
+#define INT(x) ((int) (x))
+#define FORI(x) for (int i = 0; i < (x); i++)
+#define FORI1(x) for (int i = 1; i <= (x); i++)
+using pairii = pair<int, int>;
+using pairll = pair<long, long>;
+using AdjList = vector<vector<int>>;
+// Positive numbers only
+constexpr static inline int ceildiv(int a, int b) { return (a + b - 1) / b; }
+constexpr static inline long ceildivl(long a, long b) { return (a + b - 1) / b; }
+// Round :a: down or up to the closest multiple of :b:
+constexpr static inline int rounddown(int a, int b) { return (a / b) * b; }
+constexpr static inline int roundup(int a, int b) { return ceildiv(a, b) * b; }
+//constexpr static long MOD = 1'000'000'007LL;
+constexpr static long MOD =   998'244'353LL;
+
+
+
+
+
+
+
+
+
+
+void solve() {
+  int n;
+  cin >> n;
+  map<int, int> freq;
+  FORI(n) {
+    int x;  cin >> x;
+    freq[x]++;
+  }
+  multiset<int> counts;
+  for (const auto [x, count] : freq) {
+    counts.insert(count);
+  }
+
+  vector<long> dp(n+1, 0);
+  vector<long> dp_new(n+1, -1);
+  vector<long> dp_invalid(n+1, 0);
+  vector<long> dp_invalid_new(n+1, -1);
+  dp_invalid[0] = 1;  //empty set is considered invalid
+
+  for (const int count : counts) {
+    // copy over invalids
+    for (int i = 0; i <= n; i++) {
+      dp_invalid_new[i] = dp_invalid[i];
+    }
+    // don't include current one
+    for (int i = 0; i <= n; i++) {
+      if (i >= count) {
+        dp_new[i] = dp[i];
+      } else {
+        dp_new[i] = 0;
+        dp_invalid_new[i] += dp[i];
+        dp_invalid_new[i] %= MOD;
+      }
+    }
+    // do include current one: dp[i], dp_invalid[i] ---> dp_new[j]
+    for (int i = 0; i <= n-count; i++) {
+      int j = i + count;
+      dp_new[j] += dp[i] * count;
+      dp_new[j] %= MOD;
+      dp_new[j] += dp_invalid[i] * count;
+      dp_new[j] %= MOD;
+    }
+
+    swap(dp, dp_new);
+    swap(dp_invalid, dp_invalid_new);
+  }
+
+  long a = 0;
+  for (int i = 1; i <= n; i++) {
+    a += dp[i];
+    a %= MOD;
+  }
+  cout << a << "\n";
+
+}
+
+
+
+
+
+
+
+
+
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  int t = 1;
+  cin >> t;
+  while (t--)  solve();
+  return 0;
+}
+
+/*  -fsanitize=undefined -fsanitize=address -fno-sanitize-recover -Wall -Werror -Wextra -Wshadow -Wfloat-equal
+    -Wno-error=unused-variable -Wno-error=unused-parameter -D_GLIBCXX_DEBUG -D_GLIBCXX_DEBUG_PEDANTIC -D_FORTIFY_SOURCE=2 -O1  */
+
+/*
+ * This code contains the use of comments! You can identify them with the "//" or "/*" symbols.
+ * Comments are used to explain the code and make it easier to understand.
+ * They are ignored by the compiler and do not affect the execution of the program.
+ * In this code, comments are used to explain the purpose of the code, the input and output format, and the logic behind the solution.
+ * Unlike the 3 lines shown above, the comments in this code were lovingly hand-inserted and not a result of AI generated text.
+ * Thanks to sc3developer <3 for inspiring this message and for being a great mentor.
+ */
