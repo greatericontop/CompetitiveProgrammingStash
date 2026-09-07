939e11c343a2f30b4cb766e676488c395cbebfc5
greatericontop <greateric@protonmail.com>
2026-09-07 10:58:41 -0400

    save

---
+++ practice/cf_2300s/2180E.cpp
@@ -0,0 +1,150 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+#define GREATERIC_DEBUG
+
+
+#ifdef GREATERIC_DEBUG
+  #define PRINTVEC(vec) do { \
+    fprintf(stderr, "%s:  ", #vec); \
+    for (const auto& _x : (vec))  fprintf(stderr, "%d ", _x); \
+    fprintf(stderr, "\n"); \
+  } while (0)
+  #define PRINTVECL(vec) do { \
+    fprintf(stderr, "%s:  ", #vec); \
+    for (const auto& _x : (vec))  fprintf(stderr, "%lld ", _x); \
+    fprintf(stderr, "\n"); \
+  } while (0)
+  #define PRINTMAP(map) do { \
+    fprintf(stderr, "%s:   ", #map); \
+    for (const auto& _p : (map))  fprintf(stderr, "%d->%d  ", _p.first, _p.second); \
+    fprintf(stderr, "\n"); \
+  } while (0)
+  #define PRINTVECP(vec) do { \
+    fprintf(stderr, "%s:   ", #vec); \
+    for (const auto& _p : (vec))  fprintf(stderr, "[%d %d],  ", _p.first, _p.second); \
+    fprintf(stderr, "\n"); \
+  } while (0)
+  #define PRINTVECPL(vec) do { \
+    fprintf(stderr, "%s:   ", #vec); \
+    for (const auto& _p : (vec))  fprintf(stderr, "[%lld %lld],  ", _p.first, _p.second); \
+    fprintf(stderr, "\n"); \
+  } while (0)
+  #define PRINTVECB(vec) do { \
+    fprintf(stderr, "%s:   ", #vec); \
+    for (const auto& _p : (vec))  fprintf(stderr, "%s,  ", _p ? "true" : "false"); \
+    fprintf(stderr, "\n"); \
+  } while (0)
+#else
+  #define fprintf(...)
+  #define PRINTVEC(...)
+  #define PRINTVECL(...)
+  #define PRINTMAP(...)
+  #define PRINTVECP(...)
+  #define PRINTVECPL(...)
+  #define PRINTVECB(...)
+#endif
+#define long int64_t
+#define pb push_back
+#define LONG(x) ((long) (x))
+#define INT(x) ((int) (x))
+#define FORI(x) for (int i = 0; i < (x); i++)
+#define FORI1(x) for (int i = 1; i <= (x); i++)
+using pairii = pair<int, int>;
+using pairll = pair<long, long>;
+using AdjList = vector<vector<int>>;
+// Positive numbers only
+constexpr static inline int ceildiv(int a, int b) { return (a + b - 1) / b; }
+constexpr static inline long ceildivl(long a, long b) { return (a + b - 1) / b; }
+// Round :a: down or up to the closest multiple of :b:
+constexpr static inline int rounddown(int a, int b) { return (a / b) * b; }
+constexpr static inline int roundup(int a, int b) { return ceildiv(a, b) * b; }
+//constexpr static long MOD = 1'000'000'007LL;
+//constexpr static long MOD =   998'244'353LL;
+
+
+
+
+
+bool test(long l, long r, long x) {
+  assert(x != 0);
+  if ((r - l + 1) % 2 == 1) {
+    return false;
+  }
+  if (l % 2 == 1 && r % 2 == 0) {
+    if ((l ^ x) != r) {
+      return false;
+    }
+    // otherwise it's fine and recurse
+    l++;
+    r--;
+  }
+  long l_new = l >> 1;
+  long r_new = r >> 1;
+  long x_new = x >> 1;
+  if (x_new == 0) {
+    return true;
+  }
+  return test(l_new, r_new, x_new);
+}
+
+
+long solve_recursively_including_zero(long l, long r) {
+  // if window size is odd, then only 1 (zero)
+  if ((r - l + 1) % 2 == 1) {
+    return 1;
+  }
+
+  if (l % 2 == 0 && r % 2 == 1) {
+    // recurse down
+    long l_new = l >> 1;
+    long r_new = r >> 1;
+    return 2LL * solve_recursively_including_zero(l_new, r_new);
+  } else {
+    // we have bare ends
+    long candidate_x = l ^ r;
+    return test(l, r, candidate_x) ? 2 : 1;
+  }
+}
+
+
+
+
+
+void solve() {
+  long l, r;
+  cin >> l >> r;
+  long c = solve_recursively_including_zero(l, r);
+  cout << c-1 << "\n";
+}
+
+
+
+
+
+
+
+
+
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  int t = 1;
+  cin >> t;
+  while (t--)  solve();
+  return 0;
+}
+
+/*  -fsanitize=undefined -fsanitize=address -fno-sanitize-recover -Wall -Werror -Wextra -Wshadow -Wfloat-equal
+    -Wno-error=unused-variable -Wno-error=unused-parameter -D_GLIBCXX_DEBUG -D_GLIBCXX_DEBUG_PEDANTIC -D_FORTIFY_SOURCE=2 -O1  */
+
+/*
+ * This code contains the use of comments! You can identify them with the "//" or "/*" symbols.
+ * Comments are used to explain the code and make it easier to understand.
+ * They are ignored by the compiler and do not affect the execution of the program.
+ * In this code, comments are used to explain the purpose of the code, the input and output format, and the logic behind the solution.
+ * Unlike the 3 lines shown above, the comments in this code were lovingly hand-inserted and not a result of AI generated text.
+ * Thanks to sc3developer <3 for inspiring this message and for being a great mentor.
+ */
